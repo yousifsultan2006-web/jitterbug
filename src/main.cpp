035933c296d5cde6e9c23d330e40595b065036df
YousifSultan <231142289+yousifsultan2006-web@users.noreply.github.com>
2026-01-27 19:49:35 +0000

    if a is pressed, more jitterbugs spawn, limit set through vector

--- src/main.cpp
+++ src/main.cpp
@@ -3,23 +3,33 @@
 #include <bn_color.h>
 #include <bn_sprite_ptr.h>
 #include <bn_random.h>
+#include <bn_vector.h>
 
 #include "bn_sprite_items_bug.h"
+#include <bn_keypad.h>
 
 int main() {
     bn::core::init();
 
     bn::random rmg = bn::random();
 
-    bn::backdrop::set_color(bn::color(31, 0, 31));
+    bn::backdrop::set_color(bn::color(31, 0, 25));
 
-    bn::sprite_ptr badguy = bn::sprite_items::bug.create_sprite();
+    //bn::sprite_ptr badguy = bn::sprite_items::bug.create_sprite();
+
+    bn::vector<bn::sprite_ptr, 5> bugs = {};
 
     while(true) {
-        bn::fixed new_x = badguy.x() + rmg.get_fixed(-1, 1);
-        badguy.set_x(new_x);
-        bn::fixed new_y = badguy.y() + rmg.get_fixed(-1, 1);
-        badguy.set_y(new_y);
+        for(bn::sprite_ptr& badguy : bugs){
+            bn::fixed new_x = badguy.x() + rmg.get_fixed(-1, 1);
+            badguy.set_x(new_x);
+            bn::fixed new_y = badguy.y() + rmg.get_fixed(-1, 1);
+            badguy.set_y(new_y);
+        }
+        if (bn::keypad::a_pressed()) {
+            bugs.push_back(bn::sprite_items::bug.create_sprite());
+        }
+        rmg.update();
         bn::core::update();
     }
 }
