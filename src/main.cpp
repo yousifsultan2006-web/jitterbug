1db9837582a22389011a99293be8a9a012378cc9
YousifSultan <231142289+yousifsultan2006-web@users.noreply.github.com>
2026-01-27 19:20:33 +0000

    added unique sprite style, added location and jitter lines

--- src/main.cpp
+++ src/main.cpp
@@ -8,11 +8,17 @@
 int main() {
     bn::core::init();
 
+    bn::random rmg = bn::random();
+
     bn::backdrop::set_color(bn::color(31, 0, 31));
 
-    bn::sprite_ptr ladybug = bn::sprite_items::bug.create_sprite();
+    bn::sprite_ptr badguy = bn::sprite_items::bug.create_sprite();
 
     while(true) {
+        bn::fixed new_x = badguy.x() + rmg.get_fixed(-1, 1);
+        badguy.set_x(new_x);
+        bn::fixed new_y = badguy.y() + rmg.get_fixed(-1, 1);
+        badguy.set_y(new_y);
         bn::core::update();
     }
 }
